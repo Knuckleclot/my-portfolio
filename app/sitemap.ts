5f6733b30c4bfa509b68de0f801953e24b46a3cc
007lazi@gmail.com <007lazi@gmail.com>
2024-06-23 20:06:14 +0200

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `https://muizrexhepi.com`,
+      lastModified: "2024-06-20",
+    },
+  ];
+}
